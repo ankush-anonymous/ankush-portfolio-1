79630766f054784ed0bef71ed0bea6c9ce28e18a
Joel Saji Varghese <jovarghese@algomau.ca>
2025-01-13 18:23:22 -0500

    init

---
+++ backend_sanity/ankush-portfolio/schemaTypes/index.js
@@ -0,0 +1,20 @@
+import testimonials from "./testimonials"
+import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workexperience from "./workexperience"
+import works from "./works"
+
+export const schemaTypes = [
+    testimonials,
+    about,
+    brands,
+    contact,
+    experiences,
+    skills,
+    workexperience,
+    works,
+
+]
